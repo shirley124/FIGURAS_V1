c432c486b03fcf9532a556e4a8a07699086b2396
Shirley Camila <shir_mat@hotmail.com>
2021-02-15 18:47:15 +0100

    comentar codigo

--- FIGURAS_V1/Program.cs
+++ FIGURAS_V1/Program.cs
@@ -6,8 +6,8 @@ namespace FIGURAS_V1
     {
         static void Main(string[] args)
         {
-            FIGURAS fIGURAS = new FIGURAS(); //instanciando a la clase figuras
-            fIGURAS.areaCuadrado(); //accedemos al metodo creado
+            //FIGURAS fIGURAS = new FIGURAS(); //instanciando a la clase figuras
+            //fIGURAS.areaCuadrado(); //accedemos al metodo creado
             Console.ReadKey();
                 
         }
