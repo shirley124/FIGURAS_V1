0a0bd33b163f143df95ddcf9205734b6eda8bed4
Shirley Camila <shir_mat@hotmail.com>
2021-02-15 20:00:31 +0100

    añadir funcionalidad a la clase figuras y a la clase program.

--- FIGURAS_V1/FIGURAS.cs
+++ FIGURAS_V1/FIGURAS.cs
@@ -6,18 +6,35 @@ namespace FIGURAS_V1
 {
     class FIGURAS
     {
-        public decimal area { get; set;} //get set son de tipo lectura y escritura, sintaxis para declarar un atributo
-        decimal lado;
-        decimal ancho;
-        decimal radio;
+        // public decimal area { get; set;} //get set son de tipo lectura y escritura, sintaxis para declarar un atributo
+        double area = 0;
+        double lado = 0;
+        double ancho = 0;
+        double radio = 0;
 
-        public void areaCuadrado() 
+        public void areaCuadrado()  //sintaxis: visibilidad seguido de tipo de valor a retornar, seguido del nombre de la funcion
         {
             Console.WriteLine("Escribe el lado: ");
-          lado = Convert.ToDecimal(Console.ReadLine());
+          lado = Convert.ToDouble(Console.ReadLine());
             area = lado * lado;
             Console.WriteLine("El resultado del area es: " + area);
 
+        }
+        public void areaRectangulo() 
+        {
+            Console.WriteLine("Escribe el largo: ");
+            lado = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Escribe el ancho: ");
+            ancho = Convert.ToDouble(Console.ReadLine());
+            area = lado * ancho;
+            Console.WriteLine("El resultado del area es: " + area);
+        }
+        public void areaCirculo()
+        {
+            Console.WriteLine("Escribe el radio: ");
+            radio = Convert.ToDouble(Console.ReadLine());
+            area = Math.PI*Math.Pow(radio,2);
+            Console.WriteLine("El resultado del area es: " + area);
         }
 
 
