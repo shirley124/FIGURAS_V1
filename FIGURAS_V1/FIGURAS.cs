fb68f61c0cdc857999037ee60d72cb4c7c80c476
Shirley Camila <shir_mat@hotmail.com>
2021-02-15 18:43:13 +0100

    Agregar archivos de proyecto.

---
+++ FIGURAS_V1/FIGURAS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIGURAS_V1
+{
+    class FIGURAS
+    {
+        public decimal area { get; set;} //get set son de tipo lectura y escritura, sintaxis para declarar un atributo
+        decimal lado;
+        decimal ancho;
+        decimal radio;
+
+        public void areaCuadrado() 
+        {
+            Console.WriteLine("Escribe el lado: ");
+          lado = Convert.ToDecimal(Console.ReadLine());
+            area = lado * lado;
+            Console.WriteLine("El resultado del area es: " + area);
+
+        }
+
+
+    }
+}
---
+++ FIGURAS_V1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FIGURAS_V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FIGURAS fIGURAS = new FIGURAS(); //instanciando a la clase figuras
+            fIGURAS.areaCuadrado(); //accedemos al metodo creado
+            Console.ReadKey();
+                
+        }
+    }
+}
